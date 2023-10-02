c1ca079149f6e36432414c10ec4d4d2834f331f1
robotiaga <32516858+cosmogod18@users.noreply.github.com>
2023-10-02 13:58:23 +0300

    Update 1.1.1.1_Ensure_mounting_of_cramfs_filesystems_is_disabled.sh

--- 1.1.1.1_Ensure_mounting_of_cramfs_filesystems_is_disabled.sh
+++ 1.1.1.1_Ensure_mounting_of_cramfs_filesystems_is_disabled.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Description:
+# The cramfs filesystem type is a compressed read-only Linux filesystem embedded in small footprint systems. A cramfs image can be used without having to first decompressthe image.
+# Rationale:
+# Removing support for unneeded filesystem types reduces the local attack surface of the system. If this filesystem type is not needed, disable it.
+
+
 l_output=""
 l_output2=""
 l_output3=""
